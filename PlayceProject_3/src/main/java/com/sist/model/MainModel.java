13824e3ca69bed128616dc1f6210b31e298facd4
jiyun.kim <wldbs9644@gmail.com>
2022-07-29 16:24:49 +0900

    main.do(MainModel) 적용

--- PlayceProject_3/src/main/java/com/sist/model/MainModel.java
+++ PlayceProject_3/src/main/java/com/sist/model/MainModel.java
@@ -1,8 +1,16 @@
 package com.sist.model;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+
+@Controller
 public class MainModel {
-
-	
-
+	@RequestMapping("main/main.do")
+	public String main_page(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("main_jsp", "../main/home.jsp");
+		return "../main/main.jsp";
+	}
 }
--- PlayceProject_3/src/main/webapp/main/main.jsp
+++ PlayceProject_3/src/main/webapp/main/main.jsp
@@ -30,7 +30,7 @@
 </head>
 <body>
     <jsp:include page="header.jsp"></jsp:include>
-    <jsp:include page="home.jsp"></jsp:include>
+    <jsp:include page="${main_jsp }"></jsp:include>
     <jsp:include page="footer.jsp"></jsp:include>
     
     <!-- Preloader -->
